55542c3411dd65e4573c00bfdd81a646e79d4956
Kensuke Matsuoka <matsken@jp.ibm.com>
2016-08-26 18:19:27 +0900

    add config template

---
+++ config.template.js
@@ -0,0 +1,10 @@
+var config = {
+	api_token: "<slack api token>",
+	anniversary: "YYYY-MM-DD",
+	birthdays: [{name: "<Name>", date: "YYYY-MM-DD"}],
+	weatherRSS: ["http://rss.weather.yahoo.co.jp/rss/days/4410.xml",
+		"http://rss.weather.yahoo.co.jp/rss/days/4510.xml"],
+	bot_channel_id: "<bot channel id>"
+};
+
+module.exports = config;
